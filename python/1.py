8aeefa739a19a049c2152e4c06b72f2cf29eb213
Nikhil Surya Mukhi <darksublimethoughts@gmail.com>
2021-06-11 02:00:42 +0530

    added 1-2 py

---
+++ python/1.py
@@ -0,0 +1,1 @@
+print(sum([i for i in range(3, 1000, 1) if(i % 5 == 0 or i % 3 == 0)]))
---
+++ python/2.py
@@ -0,0 +1,12 @@
+def solve():
+    a, b = 1, 2
+    s = 0
+    while a < 4000000:
+        if a % 2 == 0:
+            s += a
+        a, b = b, a + b
+    return s
+
+print(solve())
+
+
