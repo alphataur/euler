e6f2047cb835e1dfe89ac8377ba32754cff67657
Nikhil Surya Mukhi <darksublimethoughts@gmail.com>
2021-06-18 18:06:45 +0530

    modified and added rust 1 4 5

--- rust/euler/src/bin/euler1.rs
+++ rust/euler/src/bin/euler1.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 fn solve() -> i64{
     let mut a: i64 = 0;
     for i in 1..1000{
@@ -8,6 +9,10 @@ fn solve() -> i64{
     a
 }
 
+fn range_solve() -> i64{
+    (1..1000).filter(|e| e % 3 == 0 || e % 5 == 0).sum()
+}
+
 fn main(){
-    println!("{}", solve())
+    println!("{}", range_solve())
 }
