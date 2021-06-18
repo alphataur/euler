bb8ec5e1b4f7a770636ed3fc53d552ea5420a72c
Nikhil Surya Mukhi <darksublimethoughts@gmail.com>
2021-06-18 23:04:18 +0530

    adding 5 clojure

---
+++ eulerclj/src/eulerclj/euler5.clj
@@ -0,0 +1,28 @@
+(ns eulerclj.euler5
+  (:gen-class))
+
+(defn gcd [a b]
+  ;use loop please!
+  (if (zero? b)
+    a
+    (gcd b (mod a b))
+  )
+)
+; gcd euclidean
+; 1920 1080
+; 840 1080
+; 1080 840
+; 240 840
+; 840 240
+; 120 240
+; 
+
+(defn lcm [a b]
+  (/ (* a b) (gcd a b))
+)
+
+
+(defn -main
+  "probably the slickest solution in clojure i've produced till now"
+  [& args]
+  (println (last (reductions lcm (range 1 20)))))
