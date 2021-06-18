bd46d204e429605dc431f64d15e546f931f9be4e
Nikhil Surya Mukhi <darksublimethoughts@gmail.com>
2021-06-18 21:45:01 +0530

    added 4 to clojure

--- eulerclj/src/eulerclj/euler3.clj
+++ eulerclj/src/eulerclj/euler3.clj
@@ -1,32 +1,34 @@
 (ns eulerclj.euler3
   (:gen-class))
 
-(defn is-factor [a b]
-  (zero? (mod a b)))
+(defn first-factor [n]
+  (loop [i (int (/ n 2))]
+    (if (< i 2)
+      1
+      (if (zero? (mod n i))
+        i
+        (recur (dec i))
+      )
+    )
+  )
+)
 
-(defn factors [n]
-  (filter #(is-factor n %) (range 2 (inc (Math/sqrt n)))))
 
-(defn- test-prime
-  "Determine if a number is prime by looping through divisors"
-  [x]
-  (loop [iter 5 top (Math/sqrt x)]
-    (cond
-      (> iter top) true
-      (or (zero? (mod x iter))
-          (zero? (mod x (+ 2 iter)))) false
-      :else (recur (+ 6 iter) top))))
+(defn is-prime [n]
+  (< (first-factor n) 2)
+)
 
-(defn is-prime
-  "Determines if a given integer is prime."
-  [x]
-  (cond
-    (<= x 3) (< 1 x)
-    (or (zero? (mod x 2))
-        (zero? (mod x 3))) false
-    :else (test-prime x)))
+(defn solver [n]
+  (
+   loop [current (int (/ n 2)) factor (first-factor n)]
+  )
+)
+
+
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (last (filter is-prime (factors 600851475143)))))
+  (println (solver 13195)))
--- eulerclj/src/eulerclj/euler4.clj
+++ eulerclj/src/eulerclj/euler4.clj
@@ -1,10 +1,18 @@
 (ns eulerclj.euler4
   (:gen-class))
 
-(defn reverse-num [n]
-  (map #(Character/digit % 10) (str n)))
+(defn is-palindrome [n]
+  (let [repr (str n)]
+   (= repr (apply str (reverse repr)))))
+
+
+(defn solve []
+  (for [i (range 100 999) j (range 100 999) :when (is-palindrome (* i j))]
+    [(* i j) i j]
+  )
+)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (reverse-num 7292824305)))
+  (println (last (sort-by #(nth % 0) (solve)))))
